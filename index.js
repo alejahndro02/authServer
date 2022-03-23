a85e398e868cf07fb233b5a855084e0be72635cf
alejahndro02 <alejahndro02@gmail.com>
2022-03-23 12:47:51 -0600

    Se declaro la peticion http

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Desde Node');
+const express = require ('express')
+
+
+// Servicor Express
+ const app = express()
+
+//  Peciion Get
+
+app.get('/',(req,res)=>{
+    console.log();
+    res.json({
+        ok:true,
+        msg:'Todo Bien',
+        uid:2321
+    })
+})
+
+ app.listen(4000, () => {
+     console.log(`Servidor corriendo por el perto ${4000}`);
+ })
