e60c46b3363b7c87d58729c2a5308793fc881d22
alejahndro02 <alejahndro02@gmail.com>
2022-03-23 13:17:52 -0600

    Se definio la ruta raiz e importando mediante el require las rutas hijas por medio del archivo routes

--- index.js
+++ index.js
@@ -4,16 +4,8 @@ const express = require ('express')
 // Servicor Express
  const app = express()
 
-//  Peciion Get
-
-app.get('/',(req,res)=>{
-    console.log();
-    res.json({
-        ok:true,
-        msg:'Todo Bien',
-        uid:2321
-    })
-})
+//Definicion de la ruta raiz, importando las rutas hijas desde auth.js
+app.use('/api/auth', require('./routes/auth'))
 
  app.listen(4000, () => {
      console.log(`Servidor corriendo por el perto ${4000}`);
