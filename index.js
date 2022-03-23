78922713ed3a0bbdddf3edfe28ba455633f94822
alejahndro02 <alejahndro02@gmail.com>
2022-03-23 14:23:27 -0600

    Se importa el cors y se declaran los middleware para el cors y el parseo del body

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require ('express')
+const express = require ('express');
+const cors    = require('cors');
 
 
 // Servicor Express
  const app = express()
+ 
+//  Cors
+app.use(cors());
+
+// Lecturaa y parseo del body
+
+app.use(express.json());
 
 //Definicion de la ruta raiz, importando las rutas hijas desde auth.js
 app.use('/api/auth', require('./routes/auth'))
