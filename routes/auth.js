62a036701ee8ec92821bb96b5acf169339e48766
alejahndro02 <alejahndro02@gmail.com>
2022-03-24 07:40:56 -0600

    se importo el metodo check desde express validator y se evaluan los campos email y password

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,26 @@
-const { Router} = require ('express');
+const { check        } = require ('express-validator');
+const { Router       } = require ('express');
 const { crearUsuario, 
         loginUsuario, 
-        renovarToken} = require('../controllers/auth');
+        renovarToken } = require('../controllers/auth');
 
 const router= Router();
 
-// Se crea nuevo usuario
+    // Se crea nuevo usuario
 router.post('/new', crearUsuario)
 
-// Login de Usuario
+    // Login de Usuario
+      //Con el metodo check se evalua que se envie el dato correspondiente  
+router.post('/', 
+    [
+            //con el metodo check y el metodo isEmail se evalua si el campo es un email
+        check('email', 'El email es obliatorio').isEmail(),
 
-router.post('/',loginUsuario)
+           /*con el metodo check y el metodo isLength se evalua si el campo tiene un a longitud minima de 6 caracteres*/
+        check('password', 'La contraseña es obliatoria').isLength({min:6})
+    ],
+    loginUsuario
+)
 
 // Validar y revalidar el token
 router.get('/renew', renovarToken)
