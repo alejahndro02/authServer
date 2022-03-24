d5e926e9ae9d2da86b88d3d3420e1aa64fc2ce55
alejahndro02 <alejahndro02@gmail.com>
2022-03-24 11:07:02 -0600

    Se importo la funcion Validarcampos del middleware validar-campos

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
-const { check        } = require ('express-validator');
-const { Router       } = require ('express');
+const { check         } = require ('express-validator');
+const { Router        } = require ('express');
 const { crearUsuario, 
         loginUsuario, 
-        renovarToken } = require('../controllers/auth');
+        renovarToken  } = require('../controllers/auth');
+const { ValidarCampos } = require('../middlewares/validar-campos');
 
 const router= Router();
 
@@ -10,19 +11,21 @@ const router= Router();
 router.post('/new',[
         check('name', 'El name es obligatorio').not().isEmpty(),        
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password es obligatorio').isLength({min:6})        
+        check('password', 'El password es obligatorio').isLength({min:6}),
+        ValidarCampos   //Middleware personalizado 
         ], crearUsuario)
 
     // Login de Usuario post(ruta, middlewares, controlador) 
       //Con el metodo check se evalua que se envie el dato correspondiente  
 router.post('/',[
     
-            //con el metodo check y el metodo isEmail se evalua si el campo es un email
-        //     check(a-Evaluar, mensaje-Mostrar, )
+          //con el metodo check y el metodo isEmail se evalua si el campo es un email
+            //check(a-Evaluar, mensaje-Mostrar, )
         check('email', 'El email es obliatorio').isEmail(),
 
            /*con el metodo check y el metodo isLength se evalua si el campo tiene un a longitud minima de 6 caracteres*/
-        check('password', 'La contraseña es obliatoria').isLength({min:6})
+        check('password', 'La contraseña es obliatoria').isLength({min:6}),
+        ValidarCampos    
     ],loginUsuario)
 
 
