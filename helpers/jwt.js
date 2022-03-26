bf0c68951a7e47b7c059aaa0a0c171668dec930a
alejahndro02 <alejahndro02@gmail.com>
2022-03-26 12:48:21 -0600

    Se agrego un archivo que genera el JWT para la app

---
+++ helpers/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (uid, nameUser) =>{
+        // Se crea el payload 
+    const payload = {uid, nameUser};
+    const secretKey= process.env.SECRET_JWT_SEED;
+        // Se crea una promesa nueva
+    return new Promise((resolve, rejct)=>{
+        
+        jwt.sign(payload, secretKey,{
+            expiresIn:'1hr'
+        }, (err, token)=>{
+            if(err){
+                console.log(err);
+                reject(err)
+            }else{
+                resolve(token)
+
+            }
+        })
+    })
+}
+module.exports={
+    generarJWT
+}
