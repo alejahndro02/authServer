d8841bb143ba2cbe21d1dcfd08e5d8138b1fbd26
alejahndro02 <alejahndro02@gmail.com>
2022-03-26 11:20:17 -0600

    Dentro de la funcion crate usuario se crea la ligica para almacenar el usuario en la db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,49 @@
 
 // Se agrega el tipado a response
-const { response } = require('express')
+const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
 // Se separa la logica del archivo de routes
-
- const crearUsuario = (req, res=response)=> {
+    // Se agrega el async para hacela una promesa y tlizar el await 
+ const crearUsuario = async(req, res=response)=> {
  
        // Se capturan los datos del req.body
-    const { name, email, password} = req.body
+    const { nameUser, email, password} = req.body
+    try {
+        // Verificar si no hay un correo igual
+    const usuario = await Usuario.findOne({email})
+    
+    if (usuario){
+        return res.status(400).json({
+            ok: false,
+            msg:'El email ya existe en la base de datos '
+        });
+    }
+    //Usuario con el modelo 
+    const usuarioDb= new Usuario(req.body);
+    
+    // Encriptar mediante Hash la contraseña
+    
+    // Generar el JsonWebToken
+
+        //Crear usuarion en la DB
+    await usuarioDb.save()
+    // Generar respuesta Exitosa
+        return res.status(201).json({
+            ok:true,
+            uid:usuarioDb.id,
+            nameUser,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg:'Algo salio mal, hable con el Administrador'
+        })
+    }
+
       // Retorna la respuesta como json
-    return res.json({
-        ok: true,
-        msg:'Crear Usuario /new'
-    })
+
 }
 
 const loginUsuario = ( req, res = response ) => {
